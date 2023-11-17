d23e866e09c8b3a21136d572df4320296782e56d
Anastasiia Prokhorova <naumtasya@gmail.com>
2023-11-17 21:43:59 +0700

    Junit4-platform

--- src/test/java/ru/netology/service/CashBackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackerServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.service;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashBackHackerServiceTest {
     CashBackHackerService service = new CashBackHackerService();
@@ -26,6 +26,31 @@ public class CashBackHackerServiceTest {
 
     @Test
     public void testRemainGreaterThanBoundary() {
+        int amount = 1500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void testRemainLessThanBoundaryJunit5() {
+        int amount = 500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainEqualToBoundaryJunit5() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainGreaterThanBoundaryJunit5() {
         int amount = 1500;
         int expected = 500;
         int actual = service.remain(amount);
